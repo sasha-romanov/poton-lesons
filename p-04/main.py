8fbabfa9e726a7370e3e34f2f2ede5d2124b1642
sasha-romanov <romanovalexandr46@gmail.com>
2024-02-10 12:01:15 +0200

    leson

---
+++ p-04/main.py
@@ -0,0 +1,33 @@
+import os
+os.system('cls')
+clear
+
+
+
+x = input('введіть 1 число')
+
+if:
+   print('число 5 <=%d <=10 ' %(x))
+else:
+    p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p05/exercise1.py
@@ -0,0 +1,43 @@
+import random
+
+print('\u2591'*40)
+print('\u2591'*4)
+print('\u2591'*40, end='\n'*2)
+attempts = 4
+rating = 0
+
+for i in range(1, attempts + 1):
+    print('step', i, end='')
+    rondom_numder = random.randint(1, 4)
+    number = int(input(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
