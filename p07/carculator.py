39a46d87fa35a6a1fb29ae5131d98712a741902e
sasha-romanov <romanovalexandr46@gmail.com>
2024-02-24 12:00:14 +0200

    leson7

---
+++ p07/carculator.py
@@ -0,0 +1,31 @@
+is_run = True
+
+while is_run:
+    print('calculator')
+    print("You can: '+', '-', '*', '/', Exit: 'x'")
+
+    action = input('action')
+
+
+    if action == 'x':
+        is_run = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p07/exercise2.py
@@ -0,0 +1,18 @@
+tuplex = (10, 2, 30, 90, 7, 50, 50, 9, 44, 30, 1)
+summa = 0
+
+for x in tuplex:
+    summa = summa + x
+
+print(summa)
+
+
+
+
+
+
+
+
+
+
+
