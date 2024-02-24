de7fedff08ab43f6c847b93cdeec220c024db8ca
sasha-romanov <romanovalexandr46@gmail.com>
2024-02-24 12:00:55 +0200

    leson7

---
+++ p07/exercise1.py
@@ -0,0 +1,53 @@
+# list1 = [1, 2, 5, 77, 0, 3]
+# print(list1)
+# print(list1[3])
+# list1.append(333)
+#print(list1)
+
+# tuplex = (111, 3, 5)
+# print(tuplex[0])
+
+user = {'name': 'Ivan', 'age': 7}
+print(user['age'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
